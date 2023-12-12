9ce28311c2bf52ee72cba8f6207bdd73c8358e00
Abanoub Sman <abanoub.sman@gmail.com>
2023-12-12 01:00:24 -0500

    Initialize React.js for client-side development.

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  /*React.StrictMode currently helps with2:
+-Identifying components with unsafe lifecycles.
+-Warning about legacy string ref API usage.
+-Warning about deprecated findDOMNode usage.
+-Detecting unexpected side effects.
+-Detecting legacy context API.*/
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
