e4f7411357d364a0ebdb5c04b063ee66ec0ea494
Abanoub Sman <abanoub.sman@gmail.com>
2023-12-24 15:02:10 -0500

    initialized the users list component.

--- client/src/user/pages/users.js
+++ client/src/user/pages/users.js
@@ -1,7 +1,16 @@
 import React from "react";
+import UsersList from "../components/UsersList";
+const Users = () => {
+  const USERS = [
+    {
+      id: 1,
+      name: "Cool",
+      image:
+        "https://lumiere-a.akamaihd.net/v1/images/a_avatarpandorapedia_neytiri_16x9_1098_01_0e7d844a.jpeg?region=0%2C0%2C1920%2C1080&width=1536",
+      places: 3,
+    },
+  ];
+  return <UsersList items ={USERS}/>;
+};
 
-const Users=()=>{
-    return <h1>Here in users </h1>
-}
-
-export default Users
+export default Users;
